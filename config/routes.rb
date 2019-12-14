d9ed98f2e3e61a5585d113dafccd2f8cdd6a736a
hildakh <h_khajehamiri@yahoo.com>
2019-12-14 18:46:23 +0000

    sales added as a resource to admin in routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except:[:edit, :update, :show]
+    resources :sales, only: [:index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

b7268d0c67cbf074d7e239579d187d90a887c746
hildakh <h_khajehamiri@yahoo.com>
2019-12-14 02:22:05 +0000

    About page created

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
+
+  get '/about' => 'about#show'
+  # The line below works the same as the one above
+  # get 'about', to: 'about#show'
 
   resources :orders, only: [:create, :show]
 

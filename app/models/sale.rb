4d71c17e93fcfce183c9ce46da7fd197aa1d0153
hildakh <h_khajehamiri@yahoo.com>
2019-12-15 05:03:46 +0000

    three different sales added to the db and status labels created.

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,16 @@
 class Sale < ActiveRecord::Base
+
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end 
+
 end

169c47a973705579216778f1fa7aca7995ce8dc6
hildakh <h_khajehamiri@yahoo.com>
2019-12-15 03:56:33 +0000

    admin sale, controller, view and model created. Simple view added for index and partial for sale

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except:[:edit, :update, :show]
-    resources :sales, only: [:index]
+    resources :sales, only: [:index, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

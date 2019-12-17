859b1377813f2dd3d8134104d4dbff452abeee1f
hildakh <h_khajehamiri@yahoo.com>
2019-12-17 05:09:06 +0000

    test draft written. validations added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations'
-end
 
-
-
-      it 'is not valid without a name' do
-      category = Category.create(name: 'Whatever')
-      product = Product.create(category: category, price: 20, quantity: 190)
-      expect(product.errors.full_messages).to include("Name can't be blank")
-    end
-
-    it 'is not valid without a price' do
-      category = Category.create(name: 'Whatever')
-      product = Product.create(category: category, quantity: 54, name: 'Stickers')
-      expect(product.errors.full_messages).to include("Price is not a number")
-    end
-
-    it 'is not valid without a quantity' do
+    it '' do
       category = Category.create(name: 'Whatever')
       product = Product.create(category: category, price: 76, name: 'Stickers')
       expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    it 'is not valid without a category' do
-      product = Product.create(name: 'Stickers', price: 36, quantity: 42)
-      expect(product.errors.full_messages).to include("Category can't be blank")
-    end
 
-    it 'should save the product if all four fields are set' do
-      category = Category.create(name: 'Whatever')
-      product = Product.create(name: 'Bean Bag Chair', price: 120, quantity: 10, category: category)
-      product.save!
-      expect(product).to be_valid
-    end
 
   end
 

5561bc87b753a26bc46e5adb13da231a51c2d107
hildakh <h_khajehamiri@yahoo.com>
2019-12-17 17:25:53 +0000

    2nd test to check pass and passconfirm matching passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations'
 
-    it 'must be created with a password and password confirmation field' do
+    it 'must fail when password and password confirmation fields inputs do not match' do
       user = User.create(first_name: 'Meli', last_name: 'Yetenekli', email: 'meliyete@yahoo.com', password: 'burasiturkiye', password_confirmation: 'burasitur')
-        
       expect(user).not_to be_valid
     end
     
+    it 'must be created when password and password confirmation fields input match' do
+      user = User.create(first_name: 'Meli', last_name: 'Yetenekli', email: 'meliyete@yahoo.com', password: 'burasiturkiye', password_confirmation: 'burasiturkiye')
+      expect(user).to be_valid
+    end
 
 end

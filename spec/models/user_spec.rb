780a235bea9ffaa6f248f5477b5f86e0b82bee68
hildakh <h_khajehamiri@yahoo.com>
2019-12-17 17:50:50 +0000

    test to check the uniqueness of the email passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations'
+  describe 'Validations' do
 
-    it 'must fail when password and password confirmation fields inputs do not match' do
-      user = User.create(first_name: 'Meli', last_name: 'Yetenekli', email: 'meliyete@yahoo.com', password: 'burasiturkiye', password_confirmation: 'burasitur')
+    it 'is not valid when password and password confirmation fields inputs do not match' do
+      user = User.create(
+        first_name: 'Meli', 
+        last_name: 'Yetenekli', 
+        email: 'meliyete@yahoo.com', 
+        password: 'burasiturkiye', 
+        password_confirmation: 'burasitur'
+      )
       expect(user).not_to be_valid
     end
     
-    it 'must be created when password and password confirmation fields input match' do
-      user = User.create(first_name: 'Meli', last_name: 'Yetenekli', email: 'meliyete@yahoo.com', password: 'burasiturkiye', password_confirmation: 'burasiturkiye')
+    it 'is valid when password and password confirmation fields input match' do
+      user = User.create(
+        first_name: 'Meli', 
+        last_name: 'Yetenekli', 
+        email: 'meliyete@yahoo.com', 
+        password: 'burasiturkiye', 
+        password_confirmation: 'burasiturkiye'
+      )
       expect(user).to be_valid
     end
 
+    it 'is not valid with a duplicate email address' do
+      user = User.create(
+        first_name: 'John', 
+        last_name: 'Smith', 
+        email: 'johnnysmitherin@yahoo.com', 
+        password: '123456', 
+        password_confirmation: '123456'
+      )
+      userinval = User.create(
+        first_name: 'Neli', 
+        last_name: 'Furtado', 
+        email: 'johnnysmitherin@yahoo.com', 
+        password: '123456', 
+        password_confirmation: '123456'
+      )
+      expect(userinval).to_not be_valid
+    end
+  end
 end

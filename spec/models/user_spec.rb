351731f3dc69d203081330a193e26613f6d2f655
hildakh <h_khajehamiri@yahoo.com>
2019-12-17 17:23:24 +0000

    user 1st test to check matching passwords passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations'
 
-    it '' do
-      category = Category.create(name: 'Whatever')
-      product = Product.create(category: category, price: 76, name: 'Stickers')
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
+    it 'must be created with a password and password confirmation field' do
+      user = User.create(first_name: 'Meli', last_name: 'Yetenekli', email: 'meliyete@yahoo.com', password: 'burasiturkiye', password_confirmation: 'burasitur')
+        
+      expect(user).not_to be_valid
     end
-
-
-
-  end
+    
 
 end

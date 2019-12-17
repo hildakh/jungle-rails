39c140a1e96b6515f282f57f7fb8d3e83a95f431
hildakh <h_khajehamiri@yahoo.com>
2019-12-17 17:56:32 +0000

    test to check the first name passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,5 +42,16 @@ RSpec.describe User, type: :model do
       )
       expect(userinval).to_not be_valid
     end
+
+    it 'is not valid without a first name' do
+      user = User.create( 
+        last_name: 'Ozhim', 
+        email: 'nagehanozhim@yahoo.com', 
+        password: 'burasiturkiye', 
+        password_confirmation: 'burasiturkiye'
+      )
+      expect(user).to_not be_valid
+    end
+
   end
 end

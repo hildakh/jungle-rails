ecfb4216f8b2a4b9c2777d8ee96565fd835c785c
hildakh <h_khajehamiri@yahoo.com>
2019-12-18 03:05:13 +0000

    home page tests passed

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -10,6 +10,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
@@ -21,7 +22,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     visit root_path
 
     # commented out b/c it's for debugging only
-    # save_screenshot('all_products.png')
+    save_screenshot('all_products_with_image.png')
 
     # VERIFY
     expect(page).to have_css 'article.product', count: 10
